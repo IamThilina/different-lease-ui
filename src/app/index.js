41f2af4cc7a96e2ef2acdd6e2c3c489909bf544e
IamThilina <333thilina@gmail.com>
2019-04-21 18:18:41 +0530

    Init project with industry standard tools & structured the skeleton

---
+++ src/app/index.js
@@ -0,0 +1,17 @@
+
+import React, { Component } from 'react';
+import logo from '../logo.svg';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="app">
+                <header className="app-header">
+                    <h2>Different Lease UI</h2>
+                </header>
+            </div>
+        );
+    }
+}
+
+export default App;
